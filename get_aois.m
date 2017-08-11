4ff1ebff51d8c0c06cb594980191fd56a6f6272f
frenkyo <frenkyo@gmail.com>
2017-08-11 11:46:22 +0100

    removed get_aoi and added get_aois (also aoi_3 now is obsolete), updated csv_reader (csv files have different number of columns), added participants list

--- get_aois.m
+++ get_aois.m
@@ -1,4 +1,5 @@
-function aoi_num = get_aoi( x, y, content )
+function [aois_num] = get_aois( x, y, content )
+    aois_num = [];
     %determine in which aoi the gaze is
     %corners are topleft(x1,y1) bottomright(x2,y2) (see EyeLink Manual)
     %-
@@ -43,132 +44,120 @@ function aoi_num = get_aoi( x, y, content )
     aoi_8 = [993 925 1113 1045];
     %nonsymbolictarget
     aoi_9 = [743 895 936 1050];
-    %-
-    %aoi 10 - 13 are different for different displayed number 
-    %get aoi 10 
-    aoi_10_location = get_events_location(content,'AOIreal');
-    %disp(content{aoi_10_location});
-    %-
-    %example of the MSG:
-    %1      2         3  4     5         6       7  8  9  10  11
-    %MSG	timestamp !V IAREA RECTANGLE AOI_num x1 y1 x2 y2  AOI_name
-    %-
-    aoi_10 = get_event_content(content{aoi_10_location}, 7, 10 );
-    aoi_10 = cellfun(@str2double,aoi_10);
-    %disp(aoi_10);
-    %get aoi 11
-    aoi_11_location = get_events_location(content,'AOIinverted');
-    %disp(content{aoi_11_location});
-    aoi_11 = get_event_content(content{aoi_11_location}, 7, 10 );
-    aoi_11 = cellfun(@str2double,aoi_11);
-    %disp(aoi_11);
-    %-
-    %note: AOIreal and AOIinverted can coincide if the numbers are
-    %sufficently near, i.e. 56 and 65
-    %-
-    %get aoi 12
-    aoi_12_location = get_events_location(content,'AOIunderestimation');
-    %disp(content{aoi_12_location});
-    aoi_12 = get_event_content(content{aoi_12_location}, 7, 10 );
-    aoi_12 = cellfun(@str2double,aoi_12);
-    %disp(aoi_12);
-    %get aoi 13
-    aoi_13_location = get_events_location(content,'AOIoverestimation');
-    %disp(content{aoi_13_location});
-    aoi_13 = get_event_content(content{aoi_13_location}, 7, 10 );
-    aoi_13 = cellfun(@str2double,aoi_13);
-    %disp(aoi_13);
-    %-
+%     %-
+%     %aoi 10 - 13 are different for different displayed number 
+%     %get aoi 10 
+%     aoi_10_location = get_events_location(content,'AOIreal');
+%     %disp(content{aoi_10_location});
+%     %-
+%     %example of the MSG:
+%     %1      2         3  4     5         6       7  8  9  10  11
+%     %MSG	timestamp !V IAREA RECTANGLE aois_num x1 y1 x2 y2  AOI_name
+%     %-
+%     aoi_10 = get_event_content(content{aoi_10_location}, 7, 10 );
+%     aoi_10 = cellfun(@str2double,aoi_10);
+%     %disp(aoi_10);
+%     %get aoi 11
+%     aoi_11_location = get_events_location(content,'AOIinverted');
+%     %disp(content{aoi_11_location});
+%     aoi_11 = get_event_content(content{aoi_11_location}, 7, 10 );
+%     aoi_11 = cellfun(@str2double,aoi_11);
+%     %disp(aoi_11);
+%     %-
+%     %note: AOIreal and AOIinverted can coincide if the numbers are
+%     %sufficently near, i.e. 56 and 65
+%     %-
+%     %get aoi 12
+%     aoi_12_location = get_events_location(content,'AOIunderestimation');
+%     %disp(content{aoi_12_location});
+%     aoi_12 = get_event_content(content{aoi_12_location}, 7, 10 );
+%     aoi_12 = cellfun(@str2double,aoi_12);
+%     %disp(aoi_12);
+%     %get aoi 13
+%     aoi_13_location = get_events_location(content,'AOIoverestimation');
+%     %disp(content{aoi_13_location});
+%     aoi_13 = get_event_content(content{aoi_13_location}, 7, 10 );
+%     aoi_13 = cellfun(@str2double,aoi_13);
+%     %disp(aoi_13);
+%     %-
     %aoi_1
     top_left = aoi_1(1:2);
     bottom_right = aoi_1(3:4);
     if (x > top_left(1)) && (x < bottom_right(1)) && (y > top_left(2)) && (y < bottom_right(2));
-        aoi_num = 1;
-        return;
+        aois_num = [aois_num 1];
     end
     %aoi_2
     top_left = aoi_2(1:2);
     bottom_right = aoi_2(3:4);
     if (x > top_left(1)) && (x < bottom_right(1)) && (y > top_left(2)) && (y < bottom_right(2));
-        aoi_num = 2;
-        return;
+        aois_num = [aois_num 2];
     end
     %aoi_3
     top_left = aoi_3(1:2);
     bottom_right = aoi_3(3:4);
     if (x > top_left(1)) && (x < bottom_right(1)) && (y > top_left(2)) && (y < bottom_right(2));
-        aoi_num = 3;
-        return;
+        aois_num = [aois_num 3];
     end
     %aoi_4
     top_left = aoi_4(1:2);
     bottom_right = aoi_4(3:4);
     if (x > top_left(1)) && (x < bottom_right(1)) && (y > top_left(2)) && (y < bottom_right(2));
-        aoi_num = 4;
-        return;
+        aois_num = [aois_num 4];
     end
     %aoi_5
     top_left = aoi_5(1:2);
     bottom_right = aoi_5(3:4);
     if (x > top_left(1)) && (x < bottom_right(1)) && (y > top_left(2)) && (y < bottom_right(2));
-        aoi_num = 5;
-        return;
+        aois_num = [aois_num 5];
     end
     %aoi_6
     top_left = aoi_6(1:2);
     bottom_right = aoi_6(3:4);
     if (x > top_left(1)) && (x < bottom_right(1)) && (y > top_left(2)) && (y < bottom_right(2));
-        aoi_num = 6;
-        return;
+        aois_num = [aois_num 6];
     end
     %aoi_7
     top_left = aoi_7(1:2);
     bottom_right = aoi_7(3:4);
     if (x > top_left(1)) && (x < bottom_right(1)) && (y > top_left(2)) && (y < bottom_right(2));
-        aoi_num = 7;
-        return;
+        aois_num = [aois_num 7];
     end
     %aoi_8
     top_left = aoi_8(1:2);
     bottom_right = aoi_8(3:4);
     if (x > top_left(1)) && (x < bottom_right(1)) && (y > top_left(2)) && (y < bottom_right(2));
-        aoi_num = 8;
-        return;
+        aois_num = [aois_num 8];
     end
     %aoi_9
     top_left = aoi_9(1:2);
     bottom_right = aoi_9(3:4);
     if (x > top_left(1)) && (x < bottom_right(1)) && (y > top_left(2)) && (y < bottom_right(2));
-        aoi_num = 9;
-        return;
+        aois_num = [aois_num 9];
     end
-    %aoi_10
-    top_left = aoi_10(1:2);
-    bottom_right = aoi_10(3:4);
-    if (x > top_left(1)) && (x < bottom_right(1)) && (y > top_left(2)) && (y < bottom_right(2));
-        aoi_num = 10;
-        return;
-    end
-    %aoi_11
-    top_left = aoi_11(1:2);
-    bottom_right = aoi_11(3:4);
-    if (x > top_left(1)) && (x < bottom_right(1)) && (y > top_left(2)) && (y < bottom_right(2));
-        aoi_num = 11;
-        return;
-    end
-    %aoi_12
-    top_left = aoi_12(1:2);
-    bottom_right = aoi_12(3:4);
-    if (x > top_left(1)) && (x < bottom_right(1)) && (y > top_left(2)) && (y < bottom_right(2));
-        aoi_num = 12;
-        return;
-    end
-    %aoi_13
-    top_left = aoi_13(1:2);
-    bottom_right = aoi_13(3:4);
-    if (x > top_left(1)) && (x < bottom_right(1)) && (y > top_left(2)) && (y < bottom_right(2));
-        aoi_num = 13;
-        return;
-    end
+%     %aoi_10
+%     top_left = aoi_10(1:2);
+%     bottom_right = aoi_10(3:4);
+%     if (x > top_left(1)) && (x < bottom_right(1)) && (y > top_left(2)) && (y < bottom_right(2));
+%         aois_num = [aois_num 10];
+%     end
+%     %aoi_11
+%     top_left = aoi_11(1:2);
+%     bottom_right = aoi_11(3:4);
+%     if (x > top_left(1)) && (x < bottom_right(1)) && (y > top_left(2)) && (y < bottom_right(2));
+%         aois_num = [aois_num 11];
+%     end
+%     %aoi_12
+%     top_left = aoi_12(1:2);
+%     bottom_right = aoi_12(3:4);
+%     if (x > top_left(1)) && (x < bottom_right(1)) && (y > top_left(2)) && (y < bottom_right(2));
+%         aois_num = [aois_num 12];
+%     end
+%     %aoi_13
+%     top_left = aoi_13(1:2);
+%     bottom_right = aoi_13(3:4);
+%     if (x > top_left(1)) && (x < bottom_right(1)) && (y > top_left(2)) && (y < bottom_right(2));
+%         aois_num = [aois_num 13];
+%     end
+    return;
 end
 
--- my_csv_reader.m
+++ my_csv_reader.m
@@ -1,4 +1,5 @@
 function csv_content = my_csv_reader( csv_file_id )
+    frewind(csv_file_id);
     % '*' means skip that column
     % column            number
     % ------------------------
@@ -9,12 +10,13 @@ function csv_content = my_csv_reader( csv_file_id )
     % Condition         7
     % PAE               13
     % RealNumber        15
+    % RealnumberThatWasShownIs        16
     % TrialNumPerCond   19
     % TrialNumber       20
-    % order             123
+    % order             122
     % subject_nr        162
     % ------------------------ 
-    wanted_cols=[3 4 5 6 7 13 15 19 20 123 162];
+    wanted_cols=[1:200];
     format=[];
     for ii=1:210
         if any(ii==wanted_cols)
@@ -28,15 +30,67 @@ function csv_content = my_csv_reader( csv_file_id )
     %disp(format);
     temp_csv_content = textscan(csv_file_id, format, 'delimiter', ',');
     %--
-    csv_content.ChosenNumber = str2double(temp_csv_content{1}(2:length(temp_csv_content{1})));
-    csv_content.ChosenRT = str2double(temp_csv_content{2}(2:length(temp_csv_content{2})));
-    csv_content.ChosenX = str2double(temp_csv_content{3}(2:length(temp_csv_content{3})));
-    csv_content.ChosenY = str2double(temp_csv_content{4}(2:length(temp_csv_content{4})));
-    csv_content.Condition = temp_csv_content{5}(2:length(temp_csv_content{5}));
-    csv_content.pae = str2double(temp_csv_content{6}(2:length(temp_csv_content{6})));
-    csv_content.RealNumber = str2double(temp_csv_content{7}(2:length(temp_csv_content{7})));
-    csv_content.TrialNumPerCond = str2double(temp_csv_content{8}(2:length(temp_csv_content{8})));
-    csv_content.TrialNumber = str2double(temp_csv_content{9}(2:length(temp_csv_content{9})));
-    csv_content.order = temp_csv_content{10}(2:length(temp_csv_content{10}));
-    csv_content.subject_nr = str2double(temp_csv_content{11}(2:length(temp_csv_content{11})));
+    for ii=1:200
+%         disp(temp_csv_content{ii}(1:1));
+        if strcmp(temp_csv_content{ii}(1:1),'ChosenNumber')
+            column.ChosenNumber = ii;
+%             disp(column.Condition);
+        end
+        if strcmp(temp_csv_content{ii}(1:1),'ChosenRT')
+            column.ChosenRT = ii;
+%             disp(column.Condition);
+        end
+        if strcmp(temp_csv_content{ii}(1:1),'ChosenX')
+            column.ChosenX = ii;
+%             disp(column.Condition);
+        end
+        if strcmp(temp_csv_content{ii}(1:1),'ChosenY')
+            column.ChosenY = ii;
+%             disp(column.Condition);
+        end
+        if strcmp(temp_csv_content{ii}(1:1),'Condition')
+            column.Condition = ii;
+%             disp(column.Condition);
+        end
+        if strcmp(temp_csv_content{ii}(1:1),'PAE')
+            column.PAE = ii;
+%             disp(column.Condition);
+        end
+        if strcmp(temp_csv_content{ii}(1:1),'RealNumber')
+            column.RealNumber = ii;
+%             disp(column.RealNumber);
+        end
+        if strcmp(temp_csv_content{ii}(1:1),'RealnumberThatWasShownIs')
+            column.RealnumberThatWasShownIs = ii;
+%             disp(column.RealNumber);
+        end
+        if strcmp(temp_csv_content{ii}(1:1),'TrialNumPerCond')
+            column.TrialNumPerCond = ii;
+%             disp(column.TrialNumber);
+        end
+        if strcmp(temp_csv_content{ii}(1:1),'TrialNumber')
+            column.TrialNumber = ii;
+%             disp(column.TrialNumber);
+        end
+        if strcmp(temp_csv_content{ii}(1:1),'order')
+            column.order = ii;
+%             disp(column.order);
+        end
+        if strcmp(temp_csv_content{ii}(1:1),'subject_nr')
+            column.subject_nr = ii;
+%             disp(column.order);
+        end
+    end
+    %--
+    csv_content.ChosenNumber = str2double(temp_csv_content{column.ChosenNumber}(2:length(temp_csv_content{column.ChosenNumber})));
+    csv_content.ChosenRT = str2double(temp_csv_content{column.ChosenRT}(2:length(temp_csv_content{column.ChosenRT})));
+    csv_content.ChosenX = str2double(temp_csv_content{column.ChosenX}(2:length(temp_csv_content{column.ChosenX})));
+    csv_content.ChosenY = str2double(temp_csv_content{column.ChosenY}(2:length(temp_csv_content{column.ChosenY})));
+    csv_content.Condition = temp_csv_content{column.Condition}(2:length(temp_csv_content{column.Condition}));
+    csv_content.pae = str2double(temp_csv_content{column.PAE}(2:length(temp_csv_content{column.PAE})));
+    csv_content.RealNumber = str2double(temp_csv_content{column.RealNumber}(2:length(temp_csv_content{column.RealNumber})));
+    csv_content.TrialNumPerCond = str2double(temp_csv_content{column.TrialNumPerCond}(2:length(temp_csv_content{column.TrialNumPerCond})));
+    csv_content.TrialNumber = str2double(temp_csv_content{column.TrialNumber}(2:length(temp_csv_content{column.TrialNumber})));
+    csv_content.order = temp_csv_content{column.order}(2:length(temp_csv_content{column.order}));
+    csv_content.subject_nr = str2double(temp_csv_content{column.subject_nr}(2:length(temp_csv_content{column.subject_nr})));
 end
